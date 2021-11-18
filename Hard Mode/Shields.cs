7d856155553a6797beeb4ed71d15f33ad2aa6388
Gustavo Ribeiro <pokegustavo@gmail.com>
2021-11-17 21:37:47 -0300

    added masterhasmod check and made some fixes to the shields always using power

--- Hard Mode/Shields.cs
+++ Hard Mode/Shields.cs
@@ -18,15 +18,9 @@ namespace Hard_Mode
     { 
         static void Postfix(PLShieldGenerator __instance) 
         {
-            if (__instance != null && __instance.IsEquipped)
+            if (__instance != null && __instance.IsEquipped && Options.MasterHasMod)
             {
                 __instance.IsPowerActive = true;
-                /*
-                if (__instance.Current == __instance.CurrentMax && __instance.InputPower_Watts > __instance.CalculatedMaxPowerUsage_Watts * 0.25f)
-                {
-                    __instance.InputPower_Watts = __instance.CalculatedMaxPowerUsage_Watts * 0.25f;
-                }
-                */
                 if (__instance.Current >= __instance.CurrentMax)
                 {
                     if(__instance.RequestPowerUsage_Percent == 0 || __instance.RequestPowerUsage_Percent == 0.15f) __instance.RequestPowerUsage_Percent = 0.15f;
@@ -34,9 +28,13 @@ namespace Hard_Mode
                 }
                 if(__instance.GetPowerPercentInput() < 0.15f) 
                 {
-                    __instance.ChargeRateCurrent = __instance.ChargeRateMax * __instance.LevelMultiplier(0.5f, 1f) * __instance.GetPowerPercentInput() * -5 ;
-                    if ((__instance.Current - __instance.ShipStats.ShieldsChargeRate * Time.deltaTime <= 0 && __instance.ChargeRateCurrent < 0) || (__instance.Current >= __instance.CurrentMax && __instance.ChargeRateCurrent > 0)) __instance.ShipStats.ShieldsChargeRate = 0f;
-                    if (__instance.Current < 0) __instance.Current = 0f;
+                    __instance.ChargeRateCurrent = __instance.ChargeRateMax * __instance.LevelMultiplier(0.5f, 1f) * (1 - __instance.GetPowerPercentInput()) * -3 ;
+                    if (__instance.Current - __instance.ShipStats.ShieldsChargeRate * Time.deltaTime <= 0 && __instance.ChargeRateCurrent < 0) __instance.ShipStats.ShieldsChargeRate = 0f;
+                    if (__instance.Current < __instance.ChargeRateMax/5 && __instance.ChargeRateCurrent < 0)
+                    {
+                        __instance.Current = 0f;
+                        __instance.ChargeRateCurrent = 0;
+                    }
                 }
             }
         }
