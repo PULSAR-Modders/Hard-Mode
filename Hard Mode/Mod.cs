c8a847d48f0be78b19880aeb052a7b5b96168dd3
Gustavo Ribeiro <pokegustavo@gmail.com>
2024-09-04 10:26:42 -0300

    updated version for previous changes

--- Hard Mode/Mod.cs
+++ Hard Mode/Mod.cs
@@ -4,7 +4,7 @@ namespace Hard_Mode
     public class Mod : PulsarMod
     {
 
-        public override string Version => "1.5.0";
+        public override string Version => "1.5.1";
 
         public override string Author => "EngBot, Pokegustavo, Mest, Craziness924, 18107";
 
