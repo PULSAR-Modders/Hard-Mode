a901ccbc48d70c6a9e9a74c91696c12eb9c4fcee
pokegustavor <pokegustavo@gmail.com>
2025-08-22 16:27:14 -0300

    version change and credits

--- Hard Mode/Mod.cs
+++ Hard Mode/Mod.cs
@@ -4,9 +4,9 @@ namespace Hard_Mode
     public class Mod : PulsarMod
     {
 
-        public override string Version => "1.6";
+        public override string Version => "1.6.1";
 
-        public override string Author => "EngBot, Pokegustavo, Mest, Craziness924, 18107";
+        public override string Author => "EngBot, Pokegustavo, Mest, Craziness924, 18107, sugarbuzz1";
 
         public override string ShortDescription => "Makes your game a nightmare";
 
